574dcde9ccf2e14cfffb54f5558ace4647f41030
xiyibuqu <459444233@qq.com>
2017-09-21 20:43:09 +0800

    模板引擎重构功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
 	// NProgress.start();
 
 	// NProgress.done();
@@ -30,7 +30,10 @@ define(['jquery','cookie'],function($){
 	var loginInfo=$.cookie('loginInfo');
 	loginInfo=loginInfo&&JSON.parse(loginInfo);
 
-	$('.aside .profile img').attr('src',loginInfo.tc_avatar);
-	$('.aside .profile h4').html(loginInfo.tc_name);
+	// $('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	// $('.aside .profile h4').html(loginInfo.tc_name);
+	var tpl='<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+	var html=template.render(tpl,loginInfo);
+	$('.aside .profile').html(html);
 });
 	
