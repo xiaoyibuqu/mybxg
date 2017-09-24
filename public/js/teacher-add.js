3ddc1306158ca10b8247dfd5487fc1cda99fea1b
xiyibuqu <459444233@qq.com>
2017-09-24 11:21:47 +0800

    实现添加和编辑讲师页面共用

---
+++ public/js/teacher-add.js
@@ -0,0 +1,21 @@
+define(['jquery','template','util'],function($,template,util){
+	var tcId=util.qs('tc_id');
+	// console.log(tcId)
+	if(tcId){
+		$.ajax({
+			type:'get',
+			url:'/api/teacher/edit',
+			data:{tc_id:tcId},
+			dataType:'json',
+			success:function(data){
+				// console.log(data)
+				data.result.operate='编辑讲师';
+				var html=template('teacherTpl',data.result);
+				$('#teacherInfo').html(html)
+			}
+		});
+	}else{
+		var html=template('teacherTpl',{operate:'添加讲师'});
+		$('#teacherInfo').html(html);
+	}
+});
